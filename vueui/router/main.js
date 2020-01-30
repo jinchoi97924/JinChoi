63e4f924f8295e8f842b2d084f6fb14bce3cea96
Jin <Jin.Choi@obigo.com>
2020-01-30 16:39:24 +0900

    fileread api 추가

--- vueui/router/main.js
+++ vueui/router/main.js
@@ -3,9 +3,12 @@ var fs = require('fs');
 module.exports = function(app){
   app.get('/', function(req, res){
     res.render('index.html');
-
   });
-  app.get('/readFile', function(req, res){
-    fs.readFile();
+  app.get('/fileread', function(req, res){
+    fs.readFile('../names1.txt','utf-8', function(err, data){
+      if(err) throw err;
+      console.log(data);
+      //res.end( data );
+    });
   });
 };
